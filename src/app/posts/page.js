8f41a34c2b42b0e4a625d08052471471243e5d9c
Craig Clayton <craig__clayton@outlook.com>
2025-02-07 11:22:32 +0000

    add Link component to go to post by id

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,6 +1,7 @@
 import { fetchPosts } from '@/utils/api';
 import { formatDate } from '@/utils/dateFormatter';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const PostsPage = async () => {
   const { rows: posts } = await fetchPosts();
@@ -10,16 +11,19 @@ const PostsPage = async () => {
       <ul>
         {posts.map((post) => (
           <li key={post.id}>
-            <p>{post.username}</p>
-            <Image
-              src={post.src}
-              alt={post.heading}
-              width={300}
-              height={300}
-              // className='w-[300px] h-[3000px]'
-              // style={{ width: '300px', height: '300px' }}
-            />
-            <p>{formatDate(post.created_at)}</p>
+            <Link href={`/posts/${post.id}`}>
+              {' '}
+              <p>{post.username}</p>
+              <Image
+                src={post.src}
+                alt={post.heading}
+                width={300}
+                height={300}
+                // className='w-[300px] h-[3000px]'
+                // style={{ width: '300px', height: '300px' }}
+              />
+              <p>{formatDate(post.created_at)}</p>
+            </Link>
           </li>
         ))}
       </ul>
