5dd6f3fed1b410ca09f6180e5b8fe434b452734c
Craig Clayton <craig__clayton@outlook.com>
2025-02-07 10:48:41 +0000

    add paragraph and Link on homepage for user to know what the site is about

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,19 @@
+import Link from 'next/link';
+
 function HomePage() {
   return (
     <div>
-      <h3>This is the Home Page</h3>
+      <h3>Been to a football match lately?</h3>
+
+      {/* Credit to chatGPT for this paragraph */}
+      <p>
+        Share your experience with fellow fans! Whether it's the electric
+        atmosphere at the stadium, a stunning goal, or just the joy of being
+        part of the crowd, we'd love to hear your story. Leave a blog post about
+        your match-day memories or join the conversation by commenting on
+        someone else's blog. Let&apos;s celebrate the beautiful game together!
+      </p>
+      <Link href='/new-post'>Make a Post</Link>
     </div>
   );
 }
