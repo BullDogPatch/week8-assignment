70ad8333bb0fb9690e7df102cd36bc13a6bfb28a
Craig Clayton <craig__clayton@outlook.com>
2025-02-07 20:46:37 +0000

    remove comment

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -3,7 +3,6 @@ import { formatDate } from '@/utils/dateFormatter';
 import Image from 'next/image';
 import Link from 'next/link';
 
-// const commentTotal = fetchCommentsByPostId(id);
 const PostsPage = async () => {
   const { rows: posts } = await fetchPosts();
 
@@ -11,23 +10,31 @@ const PostsPage = async () => {
     const { rows: comments } = await fetchCommentsByPostId(id);
     return comments.length;
   };
+
   return (
-    <div>
-      <ul>
+    <div className='max-w-md m-auto'>
+      <ul className=''>
         {posts.map((post) => (
-          <li key={post.id}>
-            <Link href={`/posts/${post.id}`}>
-              <p>{post.username}</p>
+          <li key={post.id} className='m-6 flex justify-center'>
+            <Link
+              href={`/posts/${post.id}`}
+              className='flex flex-col items-center gap-4 p-4 border rounded-lg shadow-md w-fit'
+            >
+              <span className='text-lg font-semibold'>{post.username}</span>
+
               <Image
                 src={post.src}
                 alt={post.heading}
                 width={300}
                 height={300}
-                // className='w-[300px] h-[3000px]'
-                // style={{ width: '300px', height: '300px' }}
+                className='w-[300px] h-[300px] object-cover rounded-md'
               />
-              <p>{formatDate(post.created_at)}</p>
-              <p>{commentstotal(post.id)} comments</p>
+
+              <p className='text-gray-500'>{formatDate(post.created_at)}</p>
+
+              <p className='text-sm text-gray-700'>
+                {commentstotal(post.id)} comments
+              </p>
             </Link>
           </li>
         ))}
