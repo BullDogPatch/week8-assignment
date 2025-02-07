88a5349978de6376a7d248f0cb0490be976d915c
Craig Clayton <craig__clayton@outlook.com>
2025-02-07 15:38:03 +0000

    add comment total to each post on posts route

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,18 +1,22 @@
-import { fetchPosts } from '@/utils/api';
+import { fetchCommentsByPostId, fetchPosts } from '@/utils/api';
 import { formatDate } from '@/utils/dateFormatter';
 import Image from 'next/image';
 import Link from 'next/link';
 
+// const commentTotal = fetchCommentsByPostId(id);
 const PostsPage = async () => {
   const { rows: posts } = await fetchPosts();
-  console.log(posts);
+
+  const commentstotal = async (id) => {
+    const { rows: comments } = await fetchCommentsByPostId(id);
+    return comments.length;
+  };
   return (
     <div>
       <ul>
         {posts.map((post) => (
           <li key={post.id}>
             <Link href={`/posts/${post.id}`}>
-              {' '}
               <p>{post.username}</p>
               <Image
                 src={post.src}
@@ -23,6 +27,7 @@ const PostsPage = async () => {
                 // style={{ width: '300px', height: '300px' }}
               />
               <p>{formatDate(post.created_at)}</p>
+              <p>{commentstotal(post.id)} comments</p>
             </Link>
           </li>
         ))}
