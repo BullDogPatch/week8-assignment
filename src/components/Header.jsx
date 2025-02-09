5c4b53ea2fe3e06705cecaceef64d9ee58d62f30
Craig Clayton <craig__clayton@outlook.com>
2025-02-09 20:02:34 +0000

    add link to new-post in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,25 @@
+import { FaPlus } from 'react-icons/fa6';
 import Link from 'next/link';
 
 const Header = () => {
   return (
     <header className='mb-4 h-20 p-4 flex justify-between items-center border-b-4 border-blue-950'>
       <p>OFFside (The place for all footbal content)</p>
-      <Link href='/posts'>Posts</Link>
+      <div className='flex justify-center items-center'>
+        <Link
+          href='/new-post'
+          className='m-3 p-2 flex items-center rounded-xl hover:bg-slate-700'
+        >
+          <FaPlus className='mr-1' />
+          <span>Create</span>
+        </Link>
+        <Link
+          href='/posts'
+          className='m-3 p-2 flex items-center rounded-xl hover:bg-slate-700'
+        >
+          Posts
+        </Link>
+      </div>
     </header>
   );
 };
