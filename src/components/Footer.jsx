03047c463ad370064a971aa94b81abd3d8b20be3
Craig Clayton <craig__clayton@outlook.com>
2025-02-09 20:09:47 +0000

    steel footer from last week

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,17 @@
+import { FaSquareGithub } from 'react-icons/fa6';
+
 const Footer = () => {
   return (
-    <footer>
-      <p>This is a footer</p>
+    <footer className='h-[100px] flex justify-between items-center p-4'>
+      <p className='made-by'>Made with ❤️ by Craig Clayton</p>
+      <a
+        href='https://github.com/BullDogPatch/week8-assignment'
+        target='_blank'
+        rel='noopener noreferrer'
+        className='github-link'
+      >
+        <FaSquareGithub size={30} />
+      </a>
     </footer>
   );
 };
