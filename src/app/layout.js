5984bd6e570b83cc7e1ee822901129ab3f1b022f
Craig Clayton <craig__clayton@outlook.com>
2025-02-09 14:07:08 +0000

    add nextjs-toploader, it works right out the box, no messing like in react

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,8 @@
 import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+import NextTopLoader from 'nextjs-toploader';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import './globals.css';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <NextTopLoader />
         <Header />
         {children}
         <Footer />
