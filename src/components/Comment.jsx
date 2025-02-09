4cff910e7dcac8f8ddcecd83f3c7c53c7be1e407
Craig Clayton <craig__clayton@outlook.com>
2025-02-09 16:11:45 +0000

    style comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,11 +3,14 @@ import Link from 'next/link';
 
 const Comment = ({ postId, comment }) => {
   return (
-    <div className='bg-slate-500'>
-      <p>{comment.username}</p>
-      <p>{comment.description}</p>
-      <p>{formatDate(comment.created_at)}</p>
-      <Link href={`/posts/${postId}/comments/${comment.id}/edit-comment`}>
+    <div className='w-full p-2 mr-4 my-4 bg-slate-500 rounded-md relative'>
+      <p className='px-1 text-sm'>{comment.username}</p>
+      <p className='px-1'>{comment.description}</p>
+      <p className='p-1 text-xs'>{formatDate(comment.created_at)}</p>
+      <Link
+        href={`/posts/${postId}/comments/${comment.id}/edit-comment`}
+        className='text-sm absolute bottom-1 right-2 bg-green-700 rounded-md px-2 border-2 border-black'
+      >
         Edit
       </Link>
     </div>
