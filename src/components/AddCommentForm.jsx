01a35f4f3a424b5bb2b87753b2d6f36d2c7bcf13
Craig Clayton <craig__clayton@outlook.com>
2025-02-09 19:45:10 +0000

    tinker with styles

--- src/components/AddCommentForm.jsx
+++ src/components/AddCommentForm.jsx
@@ -11,16 +11,16 @@ const AddCommentForm = ({ id }) => {
           name='username'
           placeholder='Username'
           required
-          className='w-full text-gray-900 p-2 rounded-sm border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500'
+          className='w-[90%] m-auto text-gray-900 p-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500'
         />
         <textarea
           name='description'
           placeholder='Leave a comment here...'
           required
-          className='w-full text-gray-900 p-2 rounded-sm border border-gray-300 resize-none focus:outline-none focus:ring-2 focus:ring-blue-500 h-32'
+          className='w-[90%] m-auto text-gray-900 p-2 rounded-md border border-gray-300 resize-none focus:outline-none focus:ring-2 focus:ring-blue-500 h-32'
         ></textarea>
         <SubmitButton
-          classNames='w-full sm:w-[30%] mx-auto mt-4 p-2 text-white bg-red-600 rounded-md hover:bg-red-700 disabled:hover:bg-red-400 transition'
+          classNames=' sm:w-[30%] mx-auto mt-4 p-2 text-white bg-red-600 rounded-md hover:bg-red-700 disabled:hover:bg-red-400 transition'
           text='Leave Comment'
         />
       </form>
--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -4,10 +4,10 @@ import Link from 'next/link';
 const Comment = ({ postId, comment }) => {
   return (
     <div className='w-[100%] p-4 my-4 border-b-2 border-b-black text-white relative'>
-      <p className='mb-1 text-sm font-semibold text-blue-300'>
+      <p className='mb-1 text-sm font-semibold text-blue-300 hover:underline'>
         {comment.username}
       </p>
-      <p className='mb-2 text-base'>{comment.description}</p>
+      <p className='mb-2'>{comment.description}</p>
       <p className='text-xs text-gray-400'>{formatDate(comment.created_at)}</p>
       <Link
         href={`/posts/${postId}/comments/${comment.id}/edit-comment`}
